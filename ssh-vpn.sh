a2803fb314052465c250cc07984e2bd1313bef9c
ArashiTeam <66388008+EvoTeamMalaysia@users.noreply.github.com>
2023-05-29 13:20:00 +0800

    Update ssh-vpn.sh
    Catious!!!!
    Unblocked Torrent Limit Script due to self personal use on own server ,please be noted that any torrent download from DO ,Linode and OCI will caused server got copyright issue !!!!

--- ssh-vpn.sh
+++ ssh-vpn.sh
@@ -216,23 +216,6 @@ wget -O /etc/issue.net "https://raw.githubusercontent.com/EvoTeamMalaysia/AutoSc
 echo "Banner /etc/issue.net" >>/etc/ssh/sshd_config
 sed -i 's@DROPBEAR_BANNER=""@DROPBEAR_BANNER="/etc/issue.net"@g' /etc/default/dropbear
 
-# blockir torrent
-iptables -A FORWARD -m string --string "get_peers" --algo bm -j DROP
-iptables -A FORWARD -m string --string "announce_peer" --algo bm -j DROP
-iptables -A FORWARD -m string --string "find_node" --algo bm -j DROP
-iptables -A FORWARD -m string --algo bm --string "BitTorrent" -j DROP
-iptables -A FORWARD -m string --algo bm --string "BitTorrent protocol" -j DROP
-iptables -A FORWARD -m string --algo bm --string "peer_id=" -j DROP
-iptables -A FORWARD -m string --algo bm --string ".torrent" -j DROP
-iptables -A FORWARD -m string --algo bm --string "announce.php?passkey=" -j DROP
-iptables -A FORWARD -m string --algo bm --string "torrent" -j DROP
-iptables -A FORWARD -m string --algo bm --string "announce" -j DROP
-iptables -A FORWARD -m string --algo bm --string "info_hash" -j DROP
-iptables-save > /etc/iptables.up.rules
-iptables-restore -t < /etc/iptables.up.rules
-netfilter-persistent save
-netfilter-persistent reload
-
 # download script
 cd /usr/bin
 wget -O add-host "https://raw.githubusercontent.com/EvoTeamMalaysia/AutoScriptVPS/main/add-host.sh"
