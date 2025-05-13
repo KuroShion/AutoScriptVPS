e74caed60d57f039c363c856790cf3855482e44b
KuroShion <66388008+EvoTeamMalaysia@users.noreply.github.com>
2025-05-13 18:30:43 +0800

    Update info.sh
    New info layout for VPS

--- info.sh
+++ info.sh
@@ -1,5 +1,27 @@
 #!/bin/bash
 
+# Clear the screen
 clear
+
+# Display system information using neofetch
 neofetch
-echo "Script By EvoTeamMalaysia"
+
+# Display additional system information
+echo -e "\n============================================="
+echo -e "           Script Information"
+echo -e "============================================="
+echo -e "Script By: EvoTeamMalaysia"
+echo -e "Telegram: https://t.me/EvoTeamMalaysia"
+echo -e "GitHub: https://github.com/EvoTeamMalaysia"
+echo -e "============================================="
+
+# Display server information
+echo -e "\n============================================="
+echo -e "           Server Information"
+echo -e "============================================="
+echo -e "Hostname: $(hostname)"
+echo -e "Public IP: $(wget -qO- https://icanhazip.com)"
+echo -e "Operating System: $(lsb_release -d | awk -F'\t' '{print $2}')"
+echo -e "Kernel: $(uname -r)"
+echo -e "Uptime: $(uptime -p)"
+echo -e "============================================="
